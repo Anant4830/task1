723c9d573162f2264e6ef372dcf026d6a053d791
Anant4830 <anant4830@gmail.com>
2016-09-24 01:12:51 +0530

    Add files via upload

---
+++ sumof.java
@@ -0,0 +1,13 @@
+package assingment1;
+
+public class sumof {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=3;
+		int c=a+b;
+		System.out.println("sum of two no. is:\n"+c);// TODO Auto-generated method stub
+
+	}
+
+}
